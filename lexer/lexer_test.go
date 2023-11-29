a97a66c7baf8c0a998975d7d84740298bbcd2ea1
justinbather <justinbather@hotmail.com>
2023-11-29 09:10:33 -0500

    adds if, else, true, false, gt, lt, return statements

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -13,7 +13,16 @@ func TestNextToken(t *testing.T) {
   x + y;
   };
 
-  let result = add(five, ten);`
+  let result = add(five, ten);
+
+  !-/*5;
+  5 < 10 > 5;
+
+  if (5 < 10) {
+   return true
+  } else {
+  return false
+  };`
 
 	// Create a slice of test structs
 	tests := []struct {
@@ -55,6 +64,34 @@ func TestNextToken(t *testing.T) {
 		{token.COMMA, ","},
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.RBRACE, "}"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
